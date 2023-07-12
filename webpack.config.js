651451973fdb1bcfccc1c76a94aeabb2d1f8bd8e
xiewen <1123654054@qq.com>
2023-07-12 15:18:12 +0800

    webpack配置babel-loader进行打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/, //文件后缀js,ts
+        exclude: /node_modules/, //排除node_modules
+        use: {
+          loader: "babel-loader",
+          options: {
+            //presets 预先设置，使用webpack调用loader进行打包
+            presets: [["@babel/preset-env"]],
+          },
+        },
+      },
+    ],
+  },
 };
