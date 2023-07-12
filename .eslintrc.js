366ffe22676974a2626fb4aca33e193f6a011141
xiewen <1123654054@qq.com>
2023-07-12 16:01:25 +0800

    eslint配置ts和tsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,24 @@
 module.exports = {
-  extends: ["react-app"],
+  extends: ["react-app"], //检查js代码
   rules: {
     "react/jsx-uses-react": [2],
-    //提示要在jsx文件手动引入React
+    // 提示要在 JSX 文件里手动引入 React
     "react/react-in-jsx-scope": [2],
+    "no-console": [0],
   },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      extends: ["airbnb-typescript"],
+      rules: {
+        "@typescript-eslint/object-curly-spacing": [0],
+        "import/prefer-default-export": [0],
+        "no-console": [0],
+        "import/extensions": [0],
+      },
+    },
+  ],
 };
